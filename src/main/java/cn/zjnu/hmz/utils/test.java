cd4a0642aead9236ca59650ac92f7516b5c8acad
ZJNU-Hmz <1134598796@qq.com>
2017-11-09 20:04:35 +0800

    更新1

--- src/main/java/cn/zjnu/hmz/utils/test.java
+++ src/main/java/cn/zjnu/hmz/utils/test.java
@@ -12,4 +12,27 @@ public class test {
         int a = (int) v;
         return String.valueOf(a);
     }
+
+
+
+    public static void main(String[] args) {
+        String location = "中国浙江省金华市婺城区二环北路";
+        String sheng = "省";
+        String shi = "市";
+        String qu = "区";
+
+        int sheng_size = location.indexOf(sheng);
+        int shi_size = location.indexOf(shi);
+
+
+        String shengName = location.substring(sheng_size-2,sheng_size); //substr的值为hel
+        String shiName = location.substring(shi_size-2,shi_size); //substr的值为hel
+
+        System.out.println(shengName+shiName);
+
+        String location1 = shengName + shiName;
+
+        System.out.println(location1.substring(0,2) + "-" + location1.substring(2, 4));
+
+    }
 }
